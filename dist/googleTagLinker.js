cf9a0eba1ce152b093415b11d1b016a9147f875e
Giovani Ortolani Barbosa <giovaniortolanibarbosa@gmail.com>
2023-12-16 14:06:25 -0300

    Update README

--- dist/googleTagLinker.js
+++ dist/googleTagLinker.js
@@ -1,6 +1,6 @@
 /*!
 *
-*   @giovaniortolanibarbosa/google-tag-linker 0.1.1
+*   @giovaniortolanibarbosa/google-tag-linker 0.1.3
 *   https://github.com/analytics-debugger/google-tag-linker
 *   https://github.com/giovaniortolani/google-tag-linker
 *
